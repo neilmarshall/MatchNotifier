b111776c123107fa92aba7f3c82e733823fcea5a
neilmarshall <neil.marshall@dunelm.org.uk>
2021-01-22 23:31:13 +0000

    fix top-level error handling

--- main/__init__.py
+++ main/__init__.py
@@ -8,12 +8,14 @@ from email_client.email_client import send_email
 from fixture_parser.get_fixtures import get_fixtures
 
 
+# TODO: deal with fixtures that have expired, i.e. full-time scored fixtures
+# TODO: allow multiple competitions / teams in configuration
 def main(mytimer: func.TimerRequest) -> None:
-    fixtures_URL = os.environ.get("FixturesURL")
-    competition_name = os.environ.get("CompetitionName")
-    team_name = os.environ.get("TeamName")
-    fixture = get_fixtures(fixtures_URL, competition_name, team_name)
     try:
+        fixtures_URL = os.environ.get("FixturesURL")
+        competition_name = os.environ.get("CompetitionName")
+        team_name = os.environ.get("TeamName")
+        fixture = get_fixtures(fixtures_URL, competition_name, team_name)
         if fixture is not None:
             logging.info(f"Fixture found: {fixture}")
             home_team, away_team, matchdate = fixture
