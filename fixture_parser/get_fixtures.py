0d7dac63b8ee397227cd4b7279f75056367da634
neilmarshall <neil.marshall@dunelm.org.uk>
2021-01-23 11:57:01 +0000

    update 'get_fixtures' method

--- fixture_parser/get_fixtures.py
+++ fixture_parser/get_fixtures.py
@@ -2,7 +2,7 @@ import requests
 from .filter_fixtures import filter_fixtures
 from .parse_fixtures import parse_fixtures
 
-def get_fixtures(url : str, competition_name : str, team_name : str) -> tuple:
+def get_fixtures(url : str, fixture_filter : dict) -> tuple:
     """Parse and return fixtures from a URL, filtered by competition and team"""
     response = requests.get(url)
-    return filter_fixtures(parse_fixtures(response.content), competition_name, team_name)
+    return filter_fixtures(parse_fixtures(response.content), fixture_filter)
