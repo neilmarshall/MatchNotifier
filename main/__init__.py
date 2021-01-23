7824e90c4a3ff9f35f8c85c5b85bec983eafeed8
neilmarshall <neil.marshall@dunelm.org.uk>
2021-01-23 13:03:58 +0000

    incorporate changes to allow multiple competitions and teams to be monitored

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 import os
+from itertools import groupby
 
 import azure.functions as func
 from azure.data.tables import TableServiceClient
@@ -9,30 +10,32 @@ from email_client.email_client import send_email
 from fixture_parser.get_fixtures import get_fixtures
 
 
-# TODO: allow multiple competitions / teams in configuration
-# TODO: read configuration from Table Storage
 def main(mytimer: func.TimerRequest) -> None:
     try:
         table_client = TableServiceClient.from_connection_string(conn_str=os.environ["AzureWebJobsStorage"])
-        table = table_client.create_table_if_not_exists(os.environ["TableName"])
-
-        entities = table.list_entities()
+        table = table_client.get_table_client(os.environ["TableName"])
 
         fixtures_URL = os.environ.get("FixturesURL")
-        competition_name = os.environ.get("CompetitionName")
-        team_name = os.environ.get("TeamName")
-        recipient = os.environ["EmailRecipientAddress"]
 
-        fixture = get_fixtures(fixtures_URL, competition_name, team_name)
+        entities = sorted(table.list_entities(), key=lambda o: o['PartitionKey'])
+        for email, _entities in groupby(entities, key=lambda o: o['PartitionKey']):
+            fixture_filter = {}
+            grouped_entities = sorted(_entities, key=lambda o: o['CompetitionName'])
+            for competition_name, _grouped_entities in groupby(grouped_entities, key=lambda o: o['CompetitionName']):
+                team_names = [o['TeamName'] for o in _grouped_entities]
+                fixture_filter[competition_name] = team_names
 
-        if fixture is not None:
-            logging.info(f"Fixture found: {fixture}")
-            home_team, away_team, matchdate = fixture
-            subject = "Fixture Notification"
-            body = f"{home_team} vs. {away_team}, {matchdate.hour % 12}:{matchdate.minute:02}{'AM' if matchdate.hour < 12 else 'PM'}"
-            send_email(subject, body, recipient)
-        else:
-            logging.info(f"No fixtures found today for {team_name} in the {competition_name}")
+            fixtures = get_fixtures(fixtures_URL, fixture_filter)
+
+            if fixtures:
+                body = []
+                for fixture in fixtures:
+                    logging.info(f"Fixture found: {fixture}")
+                    home_team, away_team, matchdate = fixture
+                    body.append(f"{home_team} vs. {away_team}, {matchdate.hour % 12}:{matchdate.minute:02}{'AM' if matchdate.hour < 12 else 'PM'}")
+                subject = "Fixture Notification"
+                send_email(subject, '\n'.join(body), email)
+            else:
+                logging.info("No fixtures found today")
     except Exception as ex:
-        send_email("Fixture Notification - Error Encountered", "", recipient)
         logging.error(ex)
