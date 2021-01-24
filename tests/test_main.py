8d580876b74663b9a97009c7ec4b26f4b23a7b3e
neilmarshall <neil.marshall@dunelm.org.uk>
2021-01-24 15:28:38 +0000

    improve unit tests to check program deals with multiple emails correctly

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from datetime import datetime
-from unittest.mock import MagicMock, patch
+from unittest.mock import call, MagicMock, patch
 
 from main import main
 
@@ -9,9 +9,10 @@ class MainTestCase(unittest.TestCase):
     mock_environ = MagicMock(get = MagicMock(return_value = 'mock_get_fixtures_url'))
 
     mock_entities = [
-        {'PartitionKey': 'test@test.com', 'RowKey': '1', 'CompetitionName': 'Premier League', 'TeamName': 'Liverpool'},
-        {'PartitionKey': 'test@test.com', 'RowKey': '2', 'CompetitionName': 'The FA Cup', 'TeamName': 'Liverpool'},
-        {'PartitionKey': 'test@test.com', 'RowKey': '3', 'CompetitionName': 'The FA Cup', 'TeamName': 'Chelsea'}
+        {'PartitionKey': 'test1@test.com', 'RowKey': '1', 'CompetitionName': 'Premier League', 'TeamName': 'Liverpool'},
+        {'PartitionKey': 'test1@test.com', 'RowKey': '2', 'CompetitionName': 'The FA Cup', 'TeamName': 'Liverpool'},
+        {'PartitionKey': 'test1@test.com', 'RowKey': '3', 'CompetitionName': 'The FA Cup', 'TeamName': 'Chelsea'},
+        {'PartitionKey': 'test2@test.com', 'RowKey': '4', 'CompetitionName': 'Premier League', 'TeamName': 'Liverpool'}
     ]
 
     mock_table_client = MagicMock(list_entities = MagicMock(return_value = mock_entities))
@@ -20,10 +21,11 @@ class MainTestCase(unittest.TestCase):
     mock_table_service_client.from_connection_string.return_value = mock_table_service_client
     mock_table_service_client.get_table_client.return_value = mock_table_client
 
-    mock_get_fixtures = MagicMock(return_value=[
-        ('The FA Cup', 'Chelsea', 'Arsenal', datetime(2021, 1, 24, 12, 0, 0)),
-        ('Premier League', 'Burnley', 'Liverpool', datetime(2021, 1, 24, 16, 30, 0))
-    ])
+    mock_get_fixtures = MagicMock(side_effect=(
+        [('The FA Cup', 'Chelsea', 'Arsenal', datetime(2021, 1, 24, 12, 0, 0)),
+         ('Premier League', 'Burnley', 'Liverpool', datetime(2021, 1, 24, 16, 30, 0))],
+        [('Premier League', 'Burnley', 'Liverpool', datetime(2021, 1, 24, 16, 30, 0))]
+    ))
 
     @patch('main.os.environ', mock_environ)
     @patch('main.TableServiceClient', mock_table_service_client)
@@ -31,12 +33,14 @@ class MainTestCase(unittest.TestCase):
     @patch('main.send_email')
     def test_main(self, mock_send_email):
         main(None)
-        self.mock_get_fixtures.assert_called_once_with(
-            'mock_get_fixtures_url',
-            {'Premier League': ['Liverpool'], 'The FA Cup': ['Liverpool', 'Chelsea']}
-        )
-        mock_send_email.assert_called_once_with(
-            'Fixture Notification',
-            'The FA Cup - Chelsea vs. Arsenal, 12:00PM\nPremier League - Burnley vs. Liverpool, 4:30PM',
-            'test@test.com'
-        )
+        call_count = len(set(map(lambda o: o['PartitionKey'], self.mock_entities)))
+        self.assertEqual(self.mock_get_fixtures.call_count, call_count)
+        self.mock_get_fixtures.assert_has_calls((
+            call('mock_get_fixtures_url', {'Premier League': ['Liverpool'], 'The FA Cup': ['Liverpool', 'Chelsea']}),
+            call('mock_get_fixtures_url', {'Premier League': ['Liverpool']})
+        ))
+        self.assertEqual(mock_send_email.call_count, call_count)
+        mock_send_email.assert_has_calls((
+            call('Fixture Notification', 'The FA Cup - Chelsea vs. Arsenal, 12:00PM\nPremier League - Burnley vs. Liverpool, 4:30PM', 'test1@test.com'),
+            call('Fixture Notification', 'Premier League - Burnley vs. Liverpool, 4:30PM', 'test2@test.com')
+        ))
