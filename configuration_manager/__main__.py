91fdbc9894a8608c8181b7a6d949f06d829c7934
neilmarshall <neil.marshall@dunelm.org.uk>
2021-01-24 16:06:24 +0000

    add command line parser for configuration manager

--- configuration_manager/__main__.py
+++ configuration_manager/__main__.py
@@ -1,22 +1,27 @@
+import argparse
 import json
-import sys
 from uuid import uuid4
 
 from azure.data.tables import TableServiceClient
 
 
+parser = argparse.ArgumentParser(description='Command-line client for registering for match notifications')
+parser.add_argument('email_address', help='Email address to send notifications to')
+parser.add_argument('competition_name', help='Competition name for filtering receive notfications')
+parser.add_argument('team_name', help='Team name for filtering notfications')
+
 if __name__ == '__main__':
     try:
-        email, competition_name, team_name = sys.argv[1:]
+        args = parser.parse_args()
         with open('local.settings.json') as f:
             config = json.load(f)['Values']
         table_client = TableServiceClient.from_connection_string(conn_str=config['AzureWebJobsStorage'])
         table = table_client.create_table_if_not_exists(config['TableName'])
         entity = {
-            'PartitionKey': email,
+            'PartitionKey': args.email_address,
             'RowKey': str(uuid4()),
-            'CompetitionName': competition_name,
-            'TeamName': team_name
+            'CompetitionName': args.competition_name,
+            'TeamName': args.team_name
         }
         table.create_entity(entity)
     except Exception as e:
