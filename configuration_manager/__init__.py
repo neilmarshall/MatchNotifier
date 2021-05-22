615e560cbe2d5bcdfea1a07359da54abc962b7d4
neilmarshall <neil.marshall@dunelm.org.uk>
2021-05-22 09:37:25 +0100

    fixup! update project to use development storage where so configured

--- configuration_manager/__init__.py
+++ configuration_manager/__init__.py
@@ -1,10 +1,14 @@
 import json
+import os
 
 
 class Config():
-    def __init__(self, filename='local.settings.json'):
-        with open(filename) as f:
-            self.parameters = json.load(f)['Values']
+    def __init__(self):
+        try:
+            with open('local.settings.json') as f:
+                self.parameters = json.load(f)['Values']
+        except FileNotFoundError:
+            self.parameters = os.environ
 
     def get_connection_string(self):
         conn_str = self.parameters['AzureWebJobsStorage']
